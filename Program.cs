8d5e137cb74580a7f47d7c2bf4b96cd2123a77e4
MuhammadMehediHasan <mehedisohan1111@gmail.com>
2021-05-03 18:22:06 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Magic_8_Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] magicBallResponses =
+            {
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes – definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+            };
+
+            bool isRunning = true;
+            Random ran = new Random();
+            string botName = "MagicBot";
+            string UserName = "Anonymous";
+            chat(botName, "What is Your Name?\n");
+            chat("You", "");
+            UserName = Console.ReadLine().Trim();
+            if (string.IsNullOrEmpty(UserName)) UserName = "Anonymous";
+
+
+            Console.Clear();
+            
+
+            int maxLength = botName.Length > UserName.Length ? botName.Length + 2 : UserName.Length + 2;
+            SetMaxLength(maxLength);
+
+            chat("SERVER", "Welcome, " + UserName + "! Ask " + botName + " any question! \n");
+
+            while (isRunning)
+            {
+                chat(botName, "What do you want to know?\n");
+                chat(UserName, "");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                string question = Console.ReadLine().Trim();
+
+                bool isEmpty = string.IsNullOrEmpty(question);
+                string answer = "";
+                if (!isEmpty)
+                {
+
+                    chat(botName, "Thinking ...");
+                    Thread.Sleep(ran.Next(500));
+                    Console.Write("\n");
+
+
+                    
+                    answer = magicBallResponses.ElementAt(ran.Next(magicBallResponses.Length));
+                } else
+
+                {
+                    answer = "You didn't ask any question!";
+                }
+
+                chat(botName, answer + "\n");
+                
+
+
+                // Console.Write("===================\nAsk Again?");
+                // Console.Read();
+            }
+
+           
+            
+        }
+
+        static int _maxLength = 10;
+        static void SetMaxLength(int length)
+        {
+            _maxLength = length;
+        }
+        
+        static void chat(string name, string msg)
+        {
+            Thread.Sleep(5);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Thread.Sleep(5);
+            Console.Write("[");
+            Thread.Sleep(5);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Thread.Sleep(5);
+            Console.Write(name);
+            Thread.Sleep(5);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Thread.Sleep(5);
+            Console.Write("]");
+            Thread.Sleep(5);
+            Console.Write(" ".PadLeft(_maxLength - name.Length) + ": ");
+            Thread.Sleep(5);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            for(int i = 0; i < msg.Length; i++)
+            {
+                Thread.Sleep(30);
+                Console.Write(msg[i]);
+            }
+            Thread.Sleep(5);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Thread.Sleep(5);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Magic 8 Bot")]
+[assembly: AssemblyDescription("This is a simple Magic 8 ball bot! Ask any question")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Developer Text")]
+[assembly: AssemblyProduct("Magic 8 Bot")]
+[assembly: AssemblyCopyright("Copyright © 2021 Muhammad Mehedi Hasan")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63649c73-6fc7-4e7e-81d5-67ea2fa81d12")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
